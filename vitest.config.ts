a5eafc391d60e73b7bf66172ed238de1de04c6f0
joaoraphael-leao <jrlgo@ic.ufal.br>
2026-08-09 15:38:23 -0300

    test: configuração de vitest para cobertura de testes

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+      thresholds: {
+        statements: 60,
+        lines: 60,
+        functions: 60,
+        branches: 55,
+      },
+    },
+  },
+});
